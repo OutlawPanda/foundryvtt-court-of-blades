26d83b726c5bdad3994d463649d309f1570f6138
OutlawPanda <83490626+OutlawPanda@users.noreply.github.com>
2022-04-22 20:01:53 +0800

    Update blades.js

--- module/blades.js
+++ module/blades.js
@@ -5,22 +5,22 @@
  */
 
 // Import Modules
-import { registerSystemSettings } from "./module/settings.js";
-import { preloadHandlebarsTemplates } from "./module/blades-templates.js";
-import { bladesRoll, simpleRollPopup } from "./module/blades-roll.js";
-import { BladesHelpers } from "./module/blades-helpers.js";
-import { BladesLookup } from "./module/blades-lookup.js";
-import { BladesActor } from "./module/blades-actor.js";
-import { BladesActiveEffect } from "./module/blades-active-effect.js";
-import { BladesItem } from "./module/blades-item.js";
-import { BladesItemSheet } from "./module/blades-item-sheet.js";
-import { BladesActorSheet } from "./module/blades-actor-sheet.js";
-import { BladesHouseSheet } from "./module/blades-house-sheet.js";
-import { BladesClockSheet } from "./module/blades-clock-sheet.js";
-import { BladesNPCSheet } from "./module/blades-npc-sheet.js";
-import { BladesFactionSheet } from "./module/blades-faction-sheet.js";
-import * as migrations from "./module/migration.js";
-import {checkLocalizations} from "./module/localization.js";
+import { registerSystemSettings } from "./settings.js";
+import { preloadHandlebarsTemplates } from "./blades-templates.js";
+import { bladesRoll, simpleRollPopup } from "./blades-roll.js";
+import { BladesHelpers } from "./blades-helpers.js";
+import { BladesLookup } from "./blades-lookup.js";
+import { BladesActor } from "./blades-actor.js";
+import { BladesActiveEffect } from "./blades-active-effect.js";
+import { BladesItem } from "./blades-item.js";
+import { BladesItemSheet } from "./blades-item-sheet.js";
+import { BladesActorSheet } from "./blades-actor-sheet.js";
+import { BladesHouseSheet } from "./blades-house-sheet.js";
+import { BladesClockSheet } from "./blades-clock-sheet.js";
+import { BladesNPCSheet } from "./blades-npc-sheet.js";
+import { BladesFactionSheet } from "./blades-faction-sheet.js";
+import * as migrations from "./migration.js";
+import {checkLocalizations} from "./localization.js";
 
 
 window.BladesHelpers = BladesHelpers;
